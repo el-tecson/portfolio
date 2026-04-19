19ea6f446c500726aa0d715c3ea398fd3bf417d9
el-tecson <emmanuel.l.tecson@gmail.com>
2026-04-19 18:32:14 +0800

    feat: Make svgo config also remove dimensions

--- svgo.config.mjs
+++ svgo.config.mjs
@@ -1,12 +1,20 @@
 export default {
   plugins: [
     {
+      name: 'removeDimensions',
+      active: true,
+    },
+    {
       name: 'removeAttrs',
-      params: { attrs: '(fill|stroke|style)' },
+      params: {
+        attrs: '(fill|stroke|style)',
+      },
     },
     {
       name: 'addAttributesToSVGElement',
-      params: { attributes: [{ fill: 'currentColor' }] },
+      params: {
+        attributes: [{ fill: 'currentColor' }],
+      },
     },
   ],
-}
+};
