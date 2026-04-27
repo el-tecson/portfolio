1f7f0679e3b35e87f7de408a83a430b94e2f6a0b
el-tecson <emmanuel.l.tecson@gmail.com>
2026-04-27 15:39:03 +0800

    feat: Make /projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,15 @@
+import ProjectCard from "@/components/ui/project-card";
+import projectsData from "@/data/projects.json";
+
 export default function Projects() {
   return (
-    <main className="flex flex-col flex-1 justify-center bg-bg">
-
+    <main className="flex flex-col gap-[50px] justify-start bg-bg min-h-screen px-mobile-scr-mg-x py-25 pt-35 sm:px-scr-mg-x">
+      <h1 className="text-2xl text-txt font-bold">Projects</h1>
+      <div className="flex flex-col items-center gap-[50px] xl:flex-row xl:items-start flex-wrap">
+        {Object.keys(projectsData).map(projectName => (
+          <ProjectCard key={projectName} projectName={projectName} isHome={false} />
+        ))}
+      </div>
     </main>
   );
 }
