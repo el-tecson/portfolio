ec352b4fddffa68cc20ad6298d092a1752bccf6d
el-tecson <emmanuel.l.tecson@gmail.com>
2026-04-27 13:36:06 +0800

    refactor: Improve SEO efficiency of Navbar

---
+++ src/components/Navbar/NavbarLogo.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Logo } from "@/components/ui/icons";
+import { usePathname } from "next/navigation";
+import AccentText from "./AccentText";
+
+export default function NavbarLogo() {
+    const pathname = usePathname();
+    const isHome = pathname === "/";
+    return (
+        <>
+            <Logo className={`
+                logo
+                ${ isHome ? "fill-home-logo-clr" : "fill-logo-clr" }
+                w-16 h-16
+            `}/>
+            <p className={isHome ? "text-home-txt" : "text-txt"}>
+                <AccentText>E</AccentText>mmanuel <AccentText>L</AccentText>eu <AccentText>Tecson</AccentText>
+            </p>
+        </>
+    )
+}
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,49 +1,20 @@
-"use client";
-
-import Logo from "@/assets/logo.svg";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
 import MobileSidebar from "@/components/MobileSidebar";
 import NavbarList from "./NavbarList";
 import NavbarListItem from "./NavbarListItem";
-import AccentText from "./AccentText";
+import NavbarHeader from "./NavbarHeader";
+import NavbarLogo from "./NavbarLogo";
 
 export default function Navbar() {
-    const [scrolled, setScrolled] = useState(false);
-
-    const pathname = usePathname();
-
-    const isHome = pathname === "/";
-
-    useEffect(() => {
-        const handleScroll = () => setScrolled(window.scrollY > 10);
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-
     return (
-        <header
-            className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
-                scrolled
-                    ? (isHome ? "bg-home-nav-bg/90" : "bg-nav-bg/90") + " shadow-md backdrop-blur"
-                    : "bg-transparent"
-            }`}
-        >
+        <NavbarHeader>
             <nav className="
                 px-mobile-scr-mg-x py-mobile-scr-mg-y
                 sm:px-scr-mg-x sm:py-scr-mg-y
                 flex justify-between items-stretch
             ">
                 <Link href="/" rel="noopener noreferrer" className="flex flex-row gap-2.5 items-center">
-                    <Logo className={`
-                        logo
-                        ${ isHome ? "fill-home-logo-clr" : "fill-logo-clr" }
-                        w-16 h-16
-                    `} />
-                    <p className={isHome ? "text-home-txt" : "text-txt"}>
-                        <AccentText>E</AccentText>mmanuel <AccentText>L</AccentText>eu <AccentText>Tecson</AccentText>
-                    </p>
+                    <NavbarLogo />
                 </Link>
                 <MobileSidebar />
                 <NavbarList>
@@ -54,6 +25,6 @@ export default function Navbar() {
                     <NavbarListItem watchPath="/resume">Resume</NavbarListItem>
                 </NavbarList>
             </nav>
-        </header>
+        </NavbarHeader>
     )
 }
