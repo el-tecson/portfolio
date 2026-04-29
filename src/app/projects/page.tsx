e0089c5aed33d8ad76cfd1ffbe73daa836ff1cce
el-tecson <emmanuel.l.tecson@gmail.com>
2026-04-29 23:41:54 +0800

    style: Use grid instead of flex on desktop view of /projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
   return (
     <main className="flex flex-col gap-[50px] justify-start bg-bg min-h-screen px-mobile-scr-mg-x py-25 pt-35 sm:px-scr-mg-x">
       <h1 className="text-2xl text-txt font-bold">Projects</h1>
-      <div className="flex flex-col items-center gap-[50px] xl:flex-row xl:items-start flex-wrap">
+      <div className="flex flex-col items-center gap-[50px] flex-wrap xl:grid xl:grid-cols-2 xl:auto-rows-fr xl:flex-nowrap xl:items-start">
         {Object.keys(projectsData).map(projectName => (
           <ProjectCard key={projectName} projectName={projectName} isHome={false} />
         ))}
