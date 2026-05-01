b3629165fc9b6982ccfca3e1aeb64d4647361bde
el-tecson <emmanuel.l.tecson@gmail.com>
2026-05-02 06:36:41 +0800

    chore: Transition webpack config to turbopack config

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,14 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    })
-    return config
+
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
   },
 }
 
