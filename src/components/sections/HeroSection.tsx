38609f53bd5db997522d08cea85d2d6fa8916ee7
el-tecson <emmanuel.l.tecson@gmail.com>
2026-05-01 20:44:02 +0800

    style: Improve look of Hero Section

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,14 +1,29 @@
 import CTAButton from "@/components/ui/cta-button"
 import SubCTAButton from "@/components/ui/sub-cta-button"
+import { AnimatedGridPattern } from "@/components/ui/animated-grid-pattern"
 
 export default function HeroSection() {
     return (
         <>
-            <section id="hero" className="
-                bg-hero-section min-h-screen px-mobile-scr-mg-x sm:px-scr-mg-x
-                flex flex-col justify-center box-border gap-20 sm:gap-25
-            ">
-                <div className="flex flex-col gap-5 sm:gap-7.5">
+            <section
+                id="hero"
+                className="
+                    relative isolate overflow-hidden
+                    bg-hero-section min-h-screen
+                    px-mobile-scr-mg-x sm:px-scr-mg-x
+                    flex flex-col justify-center
+                    box-border gap-20 sm:gap-25
+                "
+            >
+                <AnimatedGridPattern 
+                    numSquares={37}
+                    maxOpacity={0.1}
+                    duration={10}
+                    width={80}
+                    height={80}
+                    className="mask-[radial-gradient(500px_circle_at_center,white,transparent)] absolute inset-0 -z-10"
+                />
+                <div className="relative z-10 flex flex-col gap-5 sm:gap-7.5">
                     <div className="flex flex-col">
                         <h1 className="text-home-txt font-bold text-5xl max-w-[12ch] sm:max-w-none sm:text-7xl leading-tight tracking-wider">
                             Emmanuel Leu Tecson
@@ -21,12 +36,11 @@ export default function HeroSection() {
                         I build fast, reliable web applications that solve real problems.
                     </p>
                 </div>
-                <div className="flex flex-row gap-4 items-center">
+                <div className="relative z-10 flex flex-row gap-4 items-center">
                     <CTAButton text="View Projects" href="#projects" />
                     <SubCTAButton text="Contact Me" href="/contact" />
                 </div>
             </section>
-            {/* Bottom filler to smoothly transition to About section */}
             <div className="bg-linear-to-b from-hero-section to-about-clr1 min-h-[10vh] sm:min-h-[15vh]"></div>
         </>
     )
