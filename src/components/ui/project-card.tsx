97f590774abe0a039c83e642e350505903192565
el-tecson <emmanuel.l.tecson@gmail.com>
2026-05-02 09:24:45 +0800

    style: Improve styling of project card

--- src/components/ui/project-card.tsx
+++ src/components/ui/project-card.tsx
@@ -25,16 +25,16 @@ export default function ProjectCard({ projectName, isHome }: ProjectCardProps) {
     <RouterDiv
       className={`
             flex flex-col py-7.5 px-3.75 sm:py-11.25 sm:px-7.5 shadow-md hover:shadow-lg
-            gap-5 rounded-2xl sm:rounded-4xl cursor-pointer sm:max-w-[600px] sm:gap-7.5 flex-1
+            gap-5 rounded-2xl cursor-pointer sm:max-w-[600px] sm:gap-7.5 flex-1 select-none
             ${isHome ? "bg-home-overlay text-home-txt hover:bg-home-overlay/90" : "bg-overlay text-txt hover:bg-overlay/90"}
             hover:transform-[scale(1.01)_translateY(2px)] transition duration-200 sm:min-w-[500px]
         `}
       key={projectName}
       pushTo={`/projects/${projectName}`}
     >
-      <h3 className="font-medium text-lg sm:text-[27px]">{project.title}</h3>
+      <h3 className="font-bold text-lg sm:text-[27px]">{project.title}</h3>
       <ScreenshotCarousel projectData={project} inProjectCard={true} />
-      <p className="font-light text-base sm:text-2xl normal-case tracking-normal">
+      <p className={`font-normal text-base sm:text-2xl normal-case tracking-normal ${isHome ? "text-home-txt/80" : "text-txt/80"}`}>
         {project.shortExcerpt}
       </p>
       {project.techStack && (
